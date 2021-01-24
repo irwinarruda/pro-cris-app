f7c75e4724af8208a23b9774c8ef25f9b9fcbd51
irwinarruda <arruda.irwin@outlook.com>
2021-01-24 00:32:29 -0300

    Exportar e importar encaminhados

--- src/screens/DatabaseHandler.js
+++ src/screens/DatabaseHandler.js
@@ -1,21 +1,68 @@
 import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { AuthContext } from '../components/AuthProvider';
 import globalStyles from '../components/globalStyles';
 import * as FileSystem  from 'expo-file-system';
 import * as Sharing from 'expo-sharing';
-//import * as DocumentPicker from 'expo-document-picker';
+import * as DocumentPicker from 'expo-document-picker';
+import * as SQLite from 'expo-sqlite';
 
 export default function DatabaseHandler() {
+    const { studentsAddArr } = React.useContext(AuthContext);    
+    const [checker, setChecker] = React.useState(false);
+    const [newStudentsArray, setNewStudentsArray] = React.useState([]);
+
+    React.useEffect(() => {
+        if(newStudentsArray.length > 0) studentsAddArr(newStudentsArray);        
+    }, [newStudentsArray]);
+
     async function handleExportDbPress() {
         let dbUri = `${FileSystem.documentDirectory}/SQLite/database.db`;
         await Sharing.shareAsync(dbUri);
     }
 
     async function handleUploadDbPress() {
-        //let document = await DocumentPicker.getDocumentAsync();
-        //console.log(document)
-        //await FileSystem.downloadAsync(document.uri, `${FileSystem.documentDirectory}/SQLite/database.db`)
+        try {
+            const document = await DocumentPicker.getDocumentAsync();
+
+            await FileSystem.copyAsync({
+                from: document.uri,
+                to: `${FileSystem.documentDirectory}/SQLite/newSqlite.db`
+            });
+
+            const db = SQLite.openDatabase('newSqlite.db');
+            db.exec([{ sql: 'PRAGMA foreign_keys = ON;', args: []}], false, () => console.log('foreign keys turned on'));
+            db.transaction(
+                tx => {
+                    tx.executeSql(
+                        `SELECT * FROM alunos;`,
+                        [],
+                        (_, { rows }) => {
+                            let studentsArr = []
+                            for(let i = 0; i < rows["length"]; i++) {
+                                studentsArr.push({
+                                    id: rows['_array'][i].id.toString(),
+                                    kidName: rows['_array'][i].kidName,
+                                    dateBirth: rows['_array'][i].dateBirth,
+                                    parentName: rows['_array'][i].parentName,
+                                    phoneNumber: rows['_array'][i].phoneNumber,
+                                    houseNumber: rows['_array'][i].houseNumber,
+                                    givenClasses: rows['_array'][i].givenClasses.slice_Number(', '),
+                                    price: rows['_array'][i].price,  
+                                });
+                            }
+                            setNewStudentsArray(studentsArr);
+                        },
+                        error => console.log(error)
+                    );
+                }
+            );
+
+        } catch(err) {
+            console.log(err);
+        }
     }
+
     return (
         <View style={styles.container}>
             <View style={styles.exportContainer}>
@@ -29,7 +76,7 @@ export default function DatabaseHandler() {
                 </TouchableOpacity>
             </View>
             <View style={styles.uploadContainer}>
-            <Text style={globalStyles.bold_black_18_karla}>Upload banco de dados</Text>
+                <Text style={globalStyles.bold_black_18_karla}>Upload banco de dados</Text>
                 <TouchableOpacity 
                     style={ styles.buttonContainer }
                     activeOpacity={0.7}
